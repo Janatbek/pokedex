c8b551bf1ba02dda1729727475ffa13be11f5ce2
Janatbek <janatbek@gmail.com>
2016-07-25 18:32:10 -0700

    updated readme, and modified css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pokedex
+1. jQuery function to show all pokemons from src='http://pokeapi.co/media/img/ API
+2. jQuery function to show all description about pokemons in other div. used .on build in function, because i am clicking on dinamically rendered html.
+3.has API call, and it's very slow. if you are cliking and you don't see the result it means page is loading, wait little bit.
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pokemons</title>
+</head>
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		$(document).ready(function()
+		{
+			var output = "";
+			for(var i = 1; i < 156; i++)
+			{
+				output += "<img id='" + i + "' src='http://pokeapi.co/media/img/" + i + ".png'>";
+			}	
+			$('#pokemon').html(output);
+
+			$(document).on("click", "img", function()
+			{
+				var num = $(this).attr("id");
+				$.get("http://pokeapi.co/api/v1/pokemon/" + num + "/", function(data)
+				{
+					var description = "";
+					var types = "";
+					for (var j = 0; j < data.types.length; j++)
+					{
+						types += "<li>" + data.types[j].name + "</li>";
+
+					}
+					description += "<h2>" + data.name + "</h2>" + "<img src='http://pokeapi.co/media/img/" + num + ".png'>" + "<h3>Types</h3><ul>" + types + "</ul><h3>Height</h3><p>" + data.height + "</p><h3>Weight</h3><p>" + data.weight + "</p>"
+				
+					$('#info').html(description);
+				},"json");
+				
+			})
+
+		});
+
+	</script>
+	<style type="text/css">
+		
+
+	</style>
+
+<body>
+	<div id="pokemon"></div>
+	<div id="info"></div>
+</body>
+</html>
